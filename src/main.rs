3587574e1086be53f0d1588c944c63918d861eda
remainder <1050107085@qq.com>
2023-07-06 00:07:41 +0800

    learn tokio runtime example

--- src/main.rs
+++ src/main.rs
@@ -1,53 +1,43 @@
-// use std::num::ParseIntError;
-// fn add_two(n_str: &str) -> Result <i32 , ParseIntError> {
-//     n_str.parse::<i32>().
-// }
+use crossbeam_deque::Worker;
+extern crate crossbeam_learn;
 
-// fn main(){
-//     assert_eq!(add_two("4").unwrap(),6);
-//     println!("sucess");
-// }
+fn main() {
+    // 通过闭包和函数分别实现自增。
+    // 译注：下面这行是使用函数的实现
+    fn  function            (i: i32) -> i32 { i + 1 }
 
-// use crossbeam_deque::Worker;
-// extern crate crossbeam_learn;
+    // 闭包是匿名的，这里我们将它们绑定到引用。
+    // 类型标注和函数的一样，不过类型标注和使用 `{}` 来围住函数体都是可选的。
+    // 这些匿名函数（nameless function）被赋值给合适地命名的变量。
+    let closure_annotated = |i: i32| -> i32 { i + 1 };
+    let closure_inferred  = |i     |          i + 1  ;
 
-// fn main() {
-//     // 通过闭包和函数分别实现自增。
-//     // 译注：下面这行是使用函数的实现
-//     fn  function            (i: i32) -> i32 { i + 1 }
+    // 译注：将闭包绑定到引用的说法可能不准。
+    // 据[语言参考](https://doc.rust-lang.org/beta/reference/types.html#closure-types)
+    // 闭包表达式产生的类型就是 “闭包类型”，不属于引用类型，而且确实无法对上面两个
+    // `closure_xxx` 变量解引用。
 
-//     // 闭包是匿名的，这里我们将它们绑定到引用。
-//     // 类型标注和函数的一样，不过类型标注和使用 `{}` 来围住函数体都是可选的。
-//     // 这些匿名函数（nameless function）被赋值给合适地命名的变量。
-//     let closure_annotated = |i: i32| -> i32 { i + 1 };
-//     let closure_inferred  = |i     |          i + 1  ;
+    let i = 1;
+    // 调用函数和闭包。
+    println!("function: {}", function(i));
+    println!("closure_annotated: {}", closure_annotated(i));
+    println!("closure_inferred: {}", closure_inferred(i));
 
-//     // 译注：将闭包绑定到引用的说法可能不准。
-//     // 据[语言参考](https://doc.rust-lang.org/beta/reference/types.html#closure-types)
-//     // 闭包表达式产生的类型就是 “闭包类型”，不属于引用类型，而且确实无法对上面两个
-//     // `closure_xxx` 变量解引用。
+    // 没有参数的闭包，返回一个 `i32` 类型。
+    // 返回类型是自动推导的。
+    let one = || 1;
+    println!("closure returning one: {}", one());
 
-//     let i = 1;
-//     // 调用函数和闭包。
-//     println!("function: {}", function(i));
-//     println!("closure_annotated: {}", closure_annotated(i));
-//     println!("closure_inferred: {}", closure_inferred(i));
+    let w = Worker::new_lifo();
 
-//     // 没有参数的闭包，返回一个 `i32` 类型。
-//     // 返回类型是自动推导的。
-//     let one = || 1;
-//     println!("closure returning one: {}", one());
-
-//     let w = Worker::new_lifo();
-
-//     assert!(w.is_empty());
-//     w.push(1);
-//     assert!(!w.is_empty());
-//     let _y = crossbeam_learn::add(2,3);
-//     crossbeam_learn::test_crossbeam_main();
-//     crossbeam_learn::test_channel_main();
-//     tokio_learn::test_runtime_main();
-// }
+    assert!(w.is_empty());
+    w.push(1);
+    assert!(!w.is_empty());
+    let _y = crossbeam_learn::add(2,3);
+    crossbeam_learn::test_crossbeam_main();
+    crossbeam_learn::test_channel_main();
+    tokio_learn::test_runtime_main();
+}
 
 // use mini_redis::{client, Result};
 
@@ -67,22 +57,22 @@
 //     Ok(())
 // }
 
- use threadpool::ThreadPool;
- use std::sync::mpsc::channel;
+//  use threadpool::ThreadPool;
+//  use std::sync::mpsc::channel;
 
-fn main(){
-    let n_workers = 4;
-    let n_jobs = 8;
-    let pool = ThreadPool::new(n_workers);
+// fn test_threadpool_main(){
+//     let n_workers = 4;
+//     let n_jobs = 8;
+//     let pool = ThreadPool::new(n_workers);
    
-    let (tx, rx) = channel();
-    for _ in 0..n_jobs {
-        let tx = tx.clone();
-        pool.execute(move|| {
-            tx.send(1).expect("channel will be there waiting for the pool");
-        });
-    }
+//     let (tx, rx) = channel();
+//     for _ in 0..n_jobs {
+//         let tx = tx.clone();
+//         pool.execute(move|| {
+//             tx.send(1).expect("channel will be there waiting for the pool");
+//         });
+//     }
    
-    assert_eq!(rx.iter().take(n_jobs).fold(0, |a, b| a + b), 8);
+//     assert_eq!(rx.iter().take(n_jobs).fold(0, |a, b| a + b), 8);
 
-}
+// }
--- tokio_learn/src/lib.rs
+++ tokio_learn/src/lib.rs
@@ -1,6 +1,7 @@
 use std::thread;
 use std::time::Duration;
 use tokio::runtime::Runtime;
+use chrono::Local;
 
 pub fn test_runtime_main() {
   // 在第一个线程内创建一个多线程的runtime
@@ -17,4 +18,14 @@ pub fn test_runtime_main() {
 
   t1.join().unwrap();
   t2.join().unwrap();
+  test_runtime();
+}
+
+pub fn test_runtime() {
+    let rt = Runtime::new().unwrap();
+    rt.block_on(async {
+        println!("before sleep {}", Local::now().format("%F %T.%3f"));
+        tokio::time::sleep(tokio::time::Duration::from_secs(2)).await;
+        println!("after sleep: {}", Local::now().format("%F %T.%3f"));
+    });
 }
